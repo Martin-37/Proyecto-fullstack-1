a96884b125c0067b7f71e9193158a9a9484097e1
Ñejejeje <aa.barrios@duocuc.cl>
2026-05-14 21:58:39 -0400

    maquinaria

---
+++ maquinariaPesada/src/test/java/cl/maquinaria/maquinariaPesada/MaquinariaPesadaApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.maquinaria.maquinariaPesada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaquinariaPesadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
